50bd6452781c51d420884e199455754dfd303a3a
David Miller <david.miller@magnetimarelli.com>
2018-05-04 19:05:08 -0400

    Prototype mismatch

--- RH850GH/partest.h
+++ RH850GH/partest.h
@@ -73,8 +73,8 @@
 #define partstDEFAULT_PORT_ADDRESS		( ( uint16_t ) 0x378 )
 
 void vParTestInitialise( void );
-void vParTestSetLED( UBaseType_t uxLED, BaseType_t xValue );
-void vParTestToggleLED( UBaseType_t uxLED );
+void vParTestSetLED(unsigned portBASE_TYPE uxLED, signed portBASE_TYPE xValue);
+void vParTestToggleLED(unsigned portBASE_TYPE uxLED);
 
 #endif
 
